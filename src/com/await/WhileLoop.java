e3601cf34eb4c6da11b35c92be77f95a0a0477b6
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2023-04-13 22:36:24 -0500

    WhileLoop

---
+++ src/com/await/WhileLoop.java
@@ -0,0 +1,12 @@
+package com.await;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 10) {
+            System.out.println("Hola mundo " + count);
+            count++; // Condición
+        }
+        System.out.println("Fin");
+    }
+}
