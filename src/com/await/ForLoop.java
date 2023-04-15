0c977099242612f03fdb91a92712bbd8e1c697f7
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2023-04-14 21:59:45 -0500

    Use of continue and break 2.0

--- src/com/await/ForLoop.java
+++ src/com/await/ForLoop.java
@@ -3,6 +3,8 @@ package com.await;
 public class ForLoop {
     public static void main(String[] args) {
         for (int i = 0; i <= 10; i++) {
+            if (i == 6)
+                continue;
             System.out.println(i + " Hola mundo");
         }
         System.out.println("Fin");
