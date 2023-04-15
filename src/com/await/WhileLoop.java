3f830cb854ea5254dd9e356dd45b8978a0fb2b1a
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2023-04-14 21:58:55 -0500

    Use of continue and break

--- src/com/await/WhileLoop.java
+++ src/com/await/WhileLoop.java
@@ -4,8 +4,11 @@ public class WhileLoop {
     public static void main(String[] args) {
         int count = 0;
         while (count < 10) {
-            System.out.println("Hola mundo " + count);
             count++; // Condición
+            if (count == 6)
+                break; // rompe el flujo de ejecución
+                // continue; // salta el valor 6 y continúa a la siguiente iteración, es decir el 6 no lo imprime
+            System.out.println("Hola mundo " + count);
         }
         System.out.println("Fin");
     }
