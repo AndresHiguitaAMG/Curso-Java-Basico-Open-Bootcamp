6e8f286fe2226adf6e6f11e6170098eb21d11d14
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2023-04-28 23:06:49 -0500

    IO standard

---
+++ src/com/await/ScannerMain.java
@@ -0,0 +1,16 @@
+package com.await;
+
+import java.util.Scanner;
+
+public class ScannerMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor introduce un nombre");
+        String nombre = scanner.nextLine();
+        System.out.println("Introduce un numero");
+        int numero = scanner.nextInt();
+        System.out.println("El nombre introducido es: " + nombre);
+        System.out.println("El numero introducido es: " + numero);
+        System.out.println("Hola mundo");
+    }
+}
